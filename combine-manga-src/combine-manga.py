6beceee022b1c20cca94965db09cd171927294b4
ts-vadim <tsentsevitskiy.v@gmail.com>
2022-07-25 19:24:15 +0700

    Refactor, added deletion of temporary volume PDFs, added --no-repeat option

--- combine-manga-src/combine-manga.py
+++ combine-manga-src/combine-manga.py
@@ -28,7 +28,14 @@ def get_volume_order(dirname: str) -> int:
 	return int(numeric)
 
 
-def main(root: str):
+def main(args: dict):
+	'''200 IQ stuff goes here'''
+
+	root = os.path.abspath(args['FOLDER'])
+	no_repeats = args['no_repeats']
+
+	added_pdfs = list()
+
 	subfolders = [os.path.join(root, f) for f in os.listdir(root) if os.path.isdir(os.path.join(root, f))]
 	subfolders.sort(key=get_volume_order)
 
@@ -38,16 +45,20 @@ def main(root: str):
 		subfolder_pdfs = [f for f in os.listdir(os.path.join(root, subfolder)) if is_pdf(f)]
 		subfolder_pdfs.sort(key=get_page_order)
 		
+		pdfs_to_add = list()
 		for pdf in subfolder_pdfs:
-			tmp_merger.append(os.path.abspath(os.path.join(root, subfolder, pdf)))
-			#print(f"Added '{pdf}'")
+			if not (no_repeats and pdf in added_pdfs):
+				tmp_merger.append(os.path.abspath(os.path.join(root, subfolder, pdf)))
+				pdfs_to_add.append(pdf)
+		
+		added_pdfs.extend(pdfs_to_add)
 
 		with open(os.path.join(root, subfolder) + '.pdf', 'wb') as volume_pdf:
 			tmp_merger.write(volume_pdf)
 			tmp_merger.close()
 
-		from_pdf = subfolder_pdfs[0][:subfolder_pdfs[0].rindex('.')]
-		to_pdf = subfolder_pdfs[-1][:subfolder_pdfs[-1].rindex('.')]
+		from_pdf = pdfs_to_add[0][:pdfs_to_add[0].rindex('.')]
+		to_pdf = pdfs_to_add[-1][:pdfs_to_add[-1].rindex('.')]
 		print(f"Combined '{os.path.basename(subfolder)}' ({from_pdf}-{to_pdf})")
 
 	merger = PdfFileMerger()
@@ -63,14 +74,22 @@ def main(root: str):
 
 	print(f"Final PDF: '{os.path.basename(final_pdf_path)}'")
 
+	for subfolder in subfolders:
+		volume_filename = os.path.join(root, subfolder) + '.pdf'
+		os.remove(volume_filename)
+
 
 if __name__ == '__main__':
 	arg_parser = argparse.ArgumentParser(description="Combines manga pages into one PDF")
+	arg_parser.add_argument('FOLDER', help="Specify folder containing manga volumes")
 	arg_parser.add_argument(
-		'FOLDER',
-		help=f"Specify folder containing manga volumes"
+		'--no-repeats',
+		dest='no_repeats',
+		action='store_true',
+		help="Don't include pages that had already been added" +
+			 " (enable this only if you actually see repeated pages in final PDF)"
 	)
 	args = arg_parser.parse_args()
 
-	main(os.path.abspath(args.FOLDER))
+	main(vars(args))
 
--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 rm -r /usr/local/bin/combine-manga-src &> /dev/null
 rm -r /usr/local/bin/combine-manga &> /dev/null
-cp -f -r ./src /usr/local/bin/combine-manga-src
+cp -f -r ./combine-manga-src /usr/local/bin/combine-manga-src
 cp -f ./combine-manga /usr/local/bin/combine-manga
 chmod +x /usr/local/bin/combine-manga
